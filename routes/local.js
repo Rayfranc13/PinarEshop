2b381a3dcdda6938a08de135fc4c393f12443208
Rayfranc13 <rayfranc13@gmail.com>
2022-03-09 01:46:25 -0500

    first commit

--- routes/local.js
+++ routes/local.js
@@ -1,10 +1,12 @@
 const { Router } =require("express");
 const { check } = require("express-validator");
 const {getLocal,postLocal,putLocal,deleteLocal}=require('../controllers/local')
-const localPermisos=require('../middlewares/localPermisos')
+const localPermisos=require('../middlewares/localPermisos');
+const { validarToken } = require("../middlewares/validarToken");
 const router=Router()
 
 router.get('/',[
+    validarToken,
     localPermisos
 ],getLocal)
 
